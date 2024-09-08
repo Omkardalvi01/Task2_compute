6230d84a81f5d75c2154a73e05c3cc2da18900a9
Omkard0901 <160456790+Omkardalvi01@users.noreply.github.com>
2024-09-08 10:27:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,30 +17,23 @@ def prediction(dataframe):
 def main():
     st.title('Automobile Price Predictor')
 
-    symboling = st.number_input('Symboling',value=None, min_value=-3)
-    norm_loss = st.number_input('Normalised losses', value=None, min_value=0)
     make = st.selectbox('Manufacturer',options = ['alfa-romero', 'audi', 'bmw', 'chevrolet', 'dodge', 'honda',
        'isuzu', 'jaguar', 'mazda', 'mercedes-benz', 'mercury',
        'mitsubishi', 'nissan', 'peugot', 'plymouth', 'porsche', 'renault',
-       'saab', 'subaru', 'toyota', 'volkswagen', 'volvo'],placeholder="Choose an option")
-    num_of_doors = st.selectbox('Number of doors', options=[1,2,4],placeholder="Choose an option")
-    body_style = st.selectbox('Body Style',options=['convertible', 'hatchback', 'sedan', 'wagon', 'hardtop'],placeholder="Choose an option")
-    asp = st.selectbox('Aspiration', options=['std','turbo'],placeholder="Choose an option")
-    l = st.number_input('Length',value=None, min_value=0.0)
+       'saab', 'subaru', 'toyota', 'volkswagen', 'volvo'], placeholder="Choose an option")
+    body_style = st.selectbox('Body Style',options=['convertible', 'hatchback', 'sedan', 'wagon', 'hardtop'])
+    asp = st.selectbox('Aspiration', options=['std','turbo'])
+    l = st.number_input('Length',value=None, min_value=0.0,)
     w = st.number_input('Width',value=None, min_value=0.0)
-    h = st.number_input('Height', value=None, min_value=0.0)
     weight = st.number_input('Curb Weight',value=None, min_value=0.0)
-    fuel_type = st.selectbox('Fuel-Type',options=['gas','diesel'],placeholder="Choose an option")
-    fuel_sys = st.selectbox('Fuel-System',options=['mpfi', '2bbl', 'mfi', '1bbl', 'spfi', '4bbl', 'idi', 'spdi'],placeholder="Choose an option")
-    engine_loc = st.selectbox('Engine Location', options=['front', 'rear'],placeholder="Choose an option")
-    drive_wheels = st.selectbox('Drive Wheels',options=['rwd', 'fwd', '4wd'],placeholder="Choose an option")
+    fuel_type = st.selectbox('Fuel-Type',options=['gas','diesel'])
+    fuel_sys = st.selectbox('Fuel-System',options=['mpfi', '2bbl', 'mfi', '1bbl', 'spfi', '4bbl', 'idi', 'spdi'])
+    engine_loc = st.selectbox('Engine Location', options=['front', 'rear'])
+    drive_wheels = st.selectbox('Drive Wheels',options=['rwd', 'fwd', '4wd'])
     hp = st.number_input('Horsepower',value=None, min_value=0.0)
-    p_rpm = st.number_input('Peak RPM',value=None, min_value=0.0)
-    engine_type = st.selectbox('Engine Type',options=['dohc', 'ohcv', 'ohc', 'l', 'rotor', 'ohcf', 'dohcv'],placeholder="Choose an option")
-    num_of_cyliner = st.selectbox('num_of_clinders', options=[2,3,4,5,6,8,12],placeholder="Choose an option")
+    engine_type = st.selectbox('Engine Type',options=['dohc', 'ohcv', 'ohc', 'l', 'rotor', 'ohcf', 'dohcv'])
+    num_of_cyliner = st.selectbox('num_of_clinders', options=[2,3,4,5,6,8,12])
     bore = st.number_input('Bore',value=None, min_value=0.0)
-    stroke = st.number_input('Stroke',value=None, min_value=0.0)
-    compression_ratio =st.number_input('Compression Ratio',value=None, min_value=0.0)
     city_mpg = st.number_input('City MPG',value=None, min_value=0.0)
     highway_mpg = st.number_input('Highway MPG', value=None, min_value=0.0)
     whl_bs = st.number_input('Wheel Base',value=None, min_value=0.0)
@@ -64,34 +57,25 @@ def main():
 
     num_data = {
         'engine-size' : [engine_size],
-        'compression-ratio':[compression_ratio],
-        'height' : [h],
         'num-of-cylinders' : [num_of_cyliner],
-        'stroke': [stroke],
         'curb-weight' : [weight],
-        'normalized-losses' : [norm_loss],
         'width' : [w],
         'length' : [l],
         'highway-mpg' : [highway_mpg],
-        'num-of-doors' : [num_of_doors],
-        'peak-rpm' : [p_rpm],
         'wheel-base' : [whl_bs],
         'horsepower' : [hp],
         'city-mpg' : [city_mpg],
-        'bore' : [bore],
-        'symboling' : [symboling]
+        'bore' : [bore]
     }
 
     num_df = pd.DataFrame(num_data)
     num_scaled = scaler.transform(num_df)
     num_scaled_df = pd.DataFrame(num_scaled, columns=num_df.columns)
     df = pd.concat([num_scaled_df, encoded_df],axis=1)
-    order = ['symboling', 'normalized-losses', 'num-of-doors', 'wheel-base',
-       'length', 'width', 'height', 'curb-weight', 'num-of-cylinders',
-       'engine-size', 'bore', 'stroke', 'compression-ratio', 'horsepower',
-       'peak-rpm', 'city-mpg', 'highway-mpg', 'fuel-type_diesel',
-       'fuel-type_gas', 'make_alfa-romero', 'make_audi', 'make_bmw',
-       'make_chevrolet', 'make_dodge', 'make_honda', 'make_isuzu',
+    order = ['wheel-base', 'length', 'width', 'curb-weight', 'num-of-cylinders',
+       'engine-size', 'bore', 'horsepower', 'city-mpg', 'highway-mpg',
+       'fuel-type_diesel', 'fuel-type_gas', 'make_alfa-romero', 'make_audi',
+       'make_bmw', 'make_chevrolet', 'make_dodge', 'make_honda', 'make_isuzu',
        'make_jaguar', 'make_mazda', 'make_mercedes-benz', 'make_mercury',
        'make_mitsubishi', 'make_nissan', 'make_peugot', 'make_plymouth',
        'make_porsche', 'make_renault', 'make_saab', 'make_subaru',
